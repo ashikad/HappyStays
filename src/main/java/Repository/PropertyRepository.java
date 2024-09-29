de2f5449a58511c592e6cc77c413f86abc8ffc34
ashikad <ashikadrolia2008@gmail.com>
2024-09-29 13:29:49 +0530

    Adding repository classes

---
+++ src/main/java/Repository/PropertyRepository.java
@@ -0,0 +1,11 @@
+package Repository;
+
+import Entity.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+
+
+}
