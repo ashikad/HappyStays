117a438309de352afacee40631adef35ed2a4e25
ashikad <ashikadrolia2008@gmail.com>
2024-09-29 10:54:42 +0530

    Init commit

---
+++ src/main/java/com/HappyStays/HappyStays/HappyStaysApplication.java
@@ -0,0 +1,13 @@
+package com.HappyStays.HappyStays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyStaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyStaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HappyStays/HappyStays/HappyStaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HappyStays.HappyStays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyStaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
